ba8d3e6cde792ed58ed154d5f63d08f8d46efe86
Arami Benitez <abenitezr18@gmail.com>
2025-02-24 17:57:02 -0300

    Boton ya tengo cuenta conectado

--- Prueba/Prueba/RegisterViewController.swift
+++ Prueba/Prueba/RegisterViewController.swift
@@ -10,31 +10,23 @@ import UIKit
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var RegisterTitleTextLabel: UILabel!
-    
     @IBOutlet weak var RegisterNameTextField: UITextField!
-    
     @IBOutlet weak var RegisterLastNameTextField: UITextField!
-    
     @IBOutlet weak var RegisterEmailTextField: UITextField!
-    
     @IBOutlet weak var ReggisterDateTextField: UITextField!
-    
     @IBOutlet weak var RegisterPasswordTextField: UITextField!
-    
     @IBOutlet weak var RegisterRepeatPasswordTextField: UITextField!
-    
     @IBOutlet weak var RegisterCreateAccountButton: UIButton!
-    
     @IBOutlet weak var RegisterAlreadyHaveAccountButton: UIButton!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    // Acción para el botón "Registrarse"
-        @IBAction func goToLogin(_ sender: UIButton) {
-                performSegue(withIdentifier: "goToLogin", sender: self)
-            }    }
-    
+        view.backgroundColor = UIColor(red: 248/255, green: 251/255, blue: 255/255, alpha: 1.0)
+    }
 
+    @IBAction func goToLogin(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToLogin", sender: self)
+    }
 }
+
