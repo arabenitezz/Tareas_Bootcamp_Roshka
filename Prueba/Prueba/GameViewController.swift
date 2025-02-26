7d50682eaa9281f56327aa315a5551255b327a59
Arami Benitez <abenitezr18@gmail.com>
2025-02-26 13:43:45 -0300

    funcion de iniciar juego acoplada con el timer

--- Prueba/Prueba/GameViewController.swift
+++ Prueba/Prueba/GameViewController.swift
@@ -13,38 +13,50 @@ class GameViewController: UIViewController {
 
 
         @IBOutlet weak var timerLabel: UILabel!
+        @IBOutlet weak var scoreLabel: UILabel!
+        @IBOutlet weak var gameContainer: UIView!
+        @IBOutlet weak var ballButton: UIButton!
         private var timerConnector: AnyCancellable?
         private var timeRemaining: TimeInterval = 30
         private var endDate: Date?
+        private var score = 0
 
         override func viewDidLoad() {
             super.viewDidLoad()
             timerLabel.text = "30"
+            scoreLabel.text = "Puntaje: 0"
+            ballButton.isHidden = true
         }
 
         @IBAction func startTimerButtonTapped(_ sender: UIButton) {
-            startTimer()
+            startGame()
         }
 
-        private func startTimer() {
+        private func startGame() {
+            score = 0
+            scoreLabel.text = "Puntaje: \(score)"
             timeRemaining = 30
             endDate = Date().addingTimeInterval(timeRemaining)
-
-            timerConnector?.cancel() // Asegura que no haya timers previos corriendo
+            
+            ballButton.isHidden = false
+            // func para mover la pelota
+            
+            timerConnector?.cancel()
             timerConnector = Timer.publish(every: 1.0, on: .main, in: .common)
                 .autoconnect()
-                .sink { [weak self] currentDate in
+                .sink{ [weak self] currentDate in
                     guard let self, let endDate else { return }
-
+                    
                     let remaining = endDate.timeIntervalSince(currentDate)
                     if remaining > 0 {
                         self.timeRemaining = remaining
                         self.timerLabel.text = "\(Int(remaining))"
                     } else {
-                        self.timerConnector?.cancel()
-                        self.timerLabel.text = "0"
+                        // funcion para terminar el juego
                     }
+                    
                 }
+ 
         }
     }
 
