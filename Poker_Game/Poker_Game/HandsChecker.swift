018987e37fdc4d8357596c9f4e76aad6284ca851
Arami Benitez <abenitezr18@gmail.com>
2025-03-05 11:52:05 -0300

    nuevo archivo para las verificaciones

---
+++ Poker_Game/Poker_Game/HandsChecker.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+// clase para verificar las distintas posibilidades
+
+class PokerHandChecker {
+    
+    func checkRoyalFlush(hand: [Card]) -> Bool {
+        
+        return false
+        
+    }
+    
+    func checkPoker(hand: [Card]) -> Bool {
+        return false
+    }
+    
+    func checkFullHouse(hand: [Card]) -> Bool {
+        return false
+    }
+    
+    func checkFlush(hand: [Card]) -> Bool {
+        return false
+        
+    }
+    
+    func checkStraight(hand: [Card]) -> Bool {
+        return false
+        
+    }
+    
+    func checkThree(hand: [Card]) -> Bool {
+        return false
+    }
+    
+    func checkTwoPair(hand:[Card]) -> Bool {
+        return false
+    }
+    
+    func checkPair(hand:[Card]) -> Bool {
+        return false
+    }
+    
+    func checkHighCard(hand:[Card]) -> Bool {
+        return false
+    }
+}
