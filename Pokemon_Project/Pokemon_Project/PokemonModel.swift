b18b34f3e8231ad1aca8f2ba196261e08ca9928b
Arami Benitez <abenitezr18@gmail.com>
2025-03-07 16:15:54 -0300

    modelo y prueba de la clase httpclient

---
+++ Pokemon_Project/Pokemon_Project/PokemonModel.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+struct Pokemon: Codable {
+    let name: String
+    let id: Int
+    let sprites: Sprites
+    
+    struct Sprites: Codable {
+        let other: Other
+        
+        struct Other: Codable {
+            let officialArtwork: OfficialArtwork
+            
+            struct OfficialArtwork: Codable {
+                let frontDefault: String
+                
+                // Mapeamos las claves del JSON a nombres más Swift-friendly
+                enum CodingKeys: String, CodingKey {
+                    case frontDefault = "front_default"
+                }
+            }
+            
+            enum CodingKeys: String, CodingKey {
+                case officialArtwork = "official-artwork"
+            }
+        }
+    }
+}
